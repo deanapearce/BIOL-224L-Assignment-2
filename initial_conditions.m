b4921e646dfc539d473f9473927278ccf5d3cd12
Dean Pearce <dapearce@live.unc.edu>
2023-01-26 14:55:54 -0500

    fix comment in initial conditions

--- initial_conditions.m
+++ initial_conditions.m
@@ -5,5 +5,5 @@ function [t, A, B] = initial_conditions(n)
   % Initialize B to zero
   B = zeros(n);
   % create clumps of B
-  B(51:60 ,51:70) = 1; % 51<x<60; 51<y<70
-  B(61:80,71:80) = 1; % 61<x<80; 71<y<80
+  B(51:60 ,51:70) = 1; % 51<y<60; 51<x<70
+  B(61:80,71:80) = 1; % 61<y<80; 71<x<80
